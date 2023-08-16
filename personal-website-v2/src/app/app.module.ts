bbd8d5d6a06cd28de1ccd08ee36acd157fb94142
LukeWilliams <lukew0207@gmail.com>
2023-08-16 15:29:25 -0400

    Added about component and fixed styling of the navbar

--- personal-website-v2/src/app/app.module.ts
+++ personal-website-v2/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
